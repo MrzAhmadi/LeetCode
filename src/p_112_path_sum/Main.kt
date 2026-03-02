1a12c7f125af498bfe862ada47292d79a4b65473
Reza <MrzAhmadi96@Gmail.com>
2026-03-02 12:42:31 +0100

    Adds Path Sum

---
+++ src/p_112_path_sum/Main.kt
@@ -0,0 +1,46 @@
+package p_112_path_sum
+
+// Problem link : https://leetcode.com/problems/path-sum
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+fun main() {
+    val solution = Solution()
+
+    val root1 = TreeNode(5)
+    root1.left = TreeNode(4).apply {
+        left = TreeNode(11).apply {
+            left = TreeNode(7)
+            right = TreeNode(2)
+        }
+    }
+    root1.right = TreeNode(8).apply {
+        left = TreeNode(13)
+        right = TreeNode(4).apply {
+            right = TreeNode(1)
+        }
+    }
+    println(solution.hasPathSum(root1, 22))
+
+    val root2 = TreeNode(1)
+    root2.left = TreeNode(2)
+    root2.right = TreeNode(3)
+    println(solution.hasPathSum(root2, 5))
+
+    val root3: TreeNode? = null
+    println(solution.hasPathSum(root3, 0))
+}
+
+class Solution {
+    fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
+        if (root == null) return false
+        if (root.left == null && root.right == null)
+            return targetSum == root.`val`
+
+        val remaining = targetSum - root.`val`
+        return hasPathSum(root.left, remaining) || hasPathSum(root.right, remaining)
+    }
+}
