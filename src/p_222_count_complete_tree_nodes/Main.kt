6ffb6e983629be29310b1b2093b6e00d9e5c3738
Reza <MrzAhmadi96@Gmail.com>
2026-03-02 13:17:49 +0100

    Adds Count Complete Tree Nodes

---
+++ src/p_222_count_complete_tree_nodes/Main.kt
@@ -0,0 +1,33 @@
+package p_222_count_complete_tree_nodes
+
+import TreeNode
+
+// Problem link : https://leetcode.com/problems/count-complete-tree-nodes
+
+fun main() {
+    val solution = Solution()
+
+    val root1 = TreeNode(1)
+    root1.left = TreeNode(2).apply {
+        left = TreeNode(4)
+        right = TreeNode(5)
+    }
+    root1.right = TreeNode(3).apply {
+        left = TreeNode(6)
+    }
+    println(solution.countNodes(root1))
+
+    val root2: TreeNode? = null
+    println(solution.countNodes(root2))
+
+    val root3 = TreeNode(1)
+    println(solution.countNodes(root3))
+}
+
+class Solution {
+    fun countNodes(root: TreeNode?): Int {
+        if (root == null)
+            return 0
+        return countNodes(root.left) + countNodes(root.right) + 1
+    }
+}
