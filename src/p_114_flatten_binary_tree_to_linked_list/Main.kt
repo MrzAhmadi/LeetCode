5cce0731953054ffa75fb296729e7844d01bc854
Reza <MrzAhmadi96@Gmail.com>
2026-03-06 15:33:43 +0100

    Adds print for p_114_flatten_binary_tree_to_linked_list

--- src/p_114_flatten_binary_tree_to_linked_list/Main.kt
+++ src/p_114_flatten_binary_tree_to_linked_list/Main.kt
@@ -4,6 +4,30 @@ import TreeNode
 
 // Problem link : https://leetcode.com/problems/flatten-binary-tree-to-linked-list
 
+fun main() {
+    val solution = Solution()
+
+    val root1 = TreeNode(1).apply {
+        left = TreeNode(2).apply {
+            left = TreeNode(3)
+            right = TreeNode(4)
+        }
+        right = TreeNode(5).apply {
+            right = TreeNode(6)
+        }
+    }
+    solution.flatten(root1)
+    println(root1.toList())
+
+    val root2: TreeNode? = null
+    solution.flatten(root2)
+//    println(root2.toList())
+
+    val root3 = TreeNode(0)
+    solution.flatten(root3)
+    println(root3.toList())
+}
+
 class Solution {
     fun flatten(root: TreeNode?) {
         if(root == null)
